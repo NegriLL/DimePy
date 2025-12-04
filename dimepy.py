ab8d07829957420ab1a2c335b6fdba098a7833f3
Leonardo Negri <leonardo.negri@helsinki.fi>
2025-12-04 14:59:56 -0300

    Implement first half of main loop

--- dimepy.py
+++ dimepy.py
@@ -372,7 +372,7 @@ def main():
 
     q[:, 1] = [0, 0, -ebeam * beta, ebeam]
     meson_parameters["istup"][1] = -1
-    if args.ppbar == True:
+    if args.ppbar:
         meson_parameters["idup"][1] = -2212
     else:
         meson_parameters["idup"][1] = 221
@@ -383,9 +383,304 @@ def main():
     for i in range(4):
         pup[i, 1] = q[i, 1]
     pup[4, 1] = np.sqrt(q[3, 1]**2 - q[2, 1]**2 - q[1, 1]**2 - q[0,1]**2)
-    meson_parameters["vtimup"] = 0
-    meson_parameters["spinup"] = 9
+    meson_parameters["vtimup"][1] = 0
+    meson_parameters["spinup"][1] = 9
 
-    # ToDo: continue here from fortran line 497
+    # Outgoing initial info
+    meson_parameters['istup'][2] = 1
+    if args.ppbar:
+        meson_parameters['idup'][1] = -2212
+    else:
+        meson_parameters['idup'][1] = 2212
+    meson_parameters["idup"][2] = 2212
+    meson_parameters["mothup"][0, 2] = 1
+    meson_parameters["mothup"][1, 2] = 2
+    meson_parameters["icolup"][0, 2] = 0
+    meson_parameters["icolup"][1, 2] = 0
+    meson_parameters["vtimup"][2] = 0
+    meson_parameters["spinup"][2] = 9
+
+    meson_parameters["istup"][3] = 1
+    meson_parameters["idup"][3] = 2212
+    meson_parameters["mothup"][0, 3] = 1
+    meson_parameters["mothup"][1, 3] = 2
+    meson_parameters["icolup"][0, 3] = 0
+    meson_parameters["icolup"][1, 3] = 0
+    meson_parameters["vtimup"][3] = 0
+    meson_parameters["spinup"][3] = 9
+
+    # HEPEVT
+    if args.output == 'hepevt':
+
+        nmxhep = 4000
+        isthep = np.zeros(nmxhep, dtype=int)
+        idhep = np.zeros(nmxhep, dtype=int)
+        jmohep = np.zeros((2, nmxhep), dtype=int)
+        jdahep = np.zeros((2, nmxhep), dtype=int)
+        phep = np.zeros((5, nmxhep), dtype=float)
+        vhep = np.zeros((4, nmxhep), dtype=float)
+        
+        nhep = meson_parameters['nup']
+        
+        for k in range(5):
+            phep[k, 0] = pup[k, 0]
+            phep[k, 1] = pup[k, 1]
+        
+        for k in range(meson_parameters['nup']):
+            isthep[k] = meson_parameters['istup'][k]
+            idhep[k] = meson_parameters['idup'][k]
+            jmohep[0, k] = meson_parameters['mothup'][0, k]
+            jmohep[1, k] = jmohep[0, k]
+            jdahep[0, k] = 0
+            jdahep[1, k] = 0
+            vhep[0, k] = 0.0
+            vhep[1, k] = 0.0
+            vhep[2, k] = 0.0
+            vhep[3, k] = 0.0
+        
+        if args.pflag == 'rho' or args.pflag == 'phi':
+            jdahep[0, 4] = 7
+            jdahep[1, 4] = 8
+            jdahep[0, 5] = 9
+            jdahep[1, 5] = 10
+        
+        jmohep[1, 4] = 2
+        jmohep[1, 5] = 2
+    
+    # Set lmax based on unw flag
+    if args.unw:
+        lmax = 2
+    else:
+        lmax = 1
+    
+    # Main event generation loop
+    for ll in range(1, lmax + 1):
+        if ll == 2:
+            # ntotal = nev * 10
+            ntotal = 1000000000
+        
+        ip = ntotal + 1
+        
+        #######################################
+        #                                     #
+        #       START OF EVENT LOOP           #
+        #                                     #
+        #######################################
+        
+        if ll == 1:
+            print("Generating weighted events...")
+        else:
+            print("Generating unweighted events...")
+        
+        for i in range(1, ntotal + 1):
+            weight = 0.0
+            
+            ran0 = np.random.random()
+            ran1 = np.random.random()
+            ran2a = np.random.random()
+            ran3 = np.random.random()
+            ran4 = np.random.random()
+            ran5 = np.random.random()
+            ran6 = np.random.random()
+            
+            ID[0] = 2212
+            q[:, 0] = [0, 0, ebeam, ebeam]
+            
+            ID[1] = 2212
+            q[:, 1] = [0, 0, -ebeam, ebeam]
+    
+            phi1 = 2.0 * pi * ran0
+            phi2 = 2.0 * pi * ran1
+            
+            pt1sq = -np.log(ran2a) / bjac
+            pt2sq = -np.log(ran3) / bjac
+            
+            weight = (np.exp(bjac * pt1sq) * np.exp(bjac * pt2sq)) / bjac**2
+            
+            pt1 = np.array([np.sqrt(pt1sq) * np.sin(phi1), np.sqrt(pt1sq) * np.cos(phi1)])
+            pt2 = np.array([np.sqrt(pt2sq) * np.sin(phi2), np.sqrt(pt2sq) * np.cos(phi2)])
+            
+            pt1x = pt1[0]
+            pt1y = pt1[1]
+            pt2x = pt2[0]
+            pt2y = pt2[1]
+            
+            if args.pflag == 'rho':
+                while True:
+                    rm1 = np.random.random()
+                    rm2 = np.random.random()
+                    
+                    mmes0 = meson_parameters['mmes0']
+                    mwidth = meson_parameters['mwidth']
+                    msmax = mmes0 + 4.0 * mwidth
+                    msmin = 2.0 * 0.13957018
+                    
+                    almin = np.arctan(-(mmes0**2 - msmin**2) / mwidth / mmes0)
+                    almax = np.arctan(-(mmes0**2 - msmax**2) / mwidth / mmes0)
+                    
+                    al1 = almin + (almax - almin) * rm1
+                    al2 = almin + (almax - almin) * rm2
+                    
+                    mmes1 = np.sqrt(np.tan(al1) * mmes0 * mwidth + mmes0**2)
+                    mmes2 = np.sqrt(np.tan(al2) * mmes0 * mwidth + mmes0**2)
+                    
+                    weight = weight * (almax - almin)**2
+                    weight = weight * mwidth**2 * mmes0**2
+                    weight = weight * (1.0 + np.tan(al1)**2) * (1.0 + np.tan(al2)**2)
+                    weight = weight / 4.0 / mmes1 / mmes2
+                    
+                    mwidth1 = mwidth * ((1.0 - 4.0 * 0.13957018**2 / mmes1**2) /
+                                       (1.0 - 4.0 * 0.13957018**2 / mmes0**2))**1.5
+                    mwidth2 = mwidth * ((1.0 - 4.0 * 0.13957018**2 / mmes2**2) /
+                                       (1.0 - 4.0 * 0.13957018**2 / mmes0**2))**1.5
+                    
+                    if mmes1 >= 2.0 * 0.13957018 and mmes2 >= 2.0 * 0.13957018:
+                        break
+                
+                weight = weight * 2.0 * mmes0 * mmes1 * mwidth1 / pi
+                weight = weight * 2.0 * mmes0 * mmes2 * mwidth2 / pi
+                weight = weight * mmes1**2 * mmes2**2 / mmes0**4
+                weight = weight / ((mmes0**2 - mmes1**2)**2 + mmes1**4 * mwidth1**2 / mmes0**2)
+                weight = weight / ((mmes0**2 - mmes2**2)**2 + mmes2**4 * mwidth2**2 / mmes0**2)
+            
+            elif args.pflag == 'phi':
+                while True:
+                    rm1 = np.random.random()
+                    rm2 = np.random.random()
+                    
+                    mmes0 = meson_parameters['mmes0']
+                    mwidth = meson_parameters['mwidth']
+                    msmax = mmes0 + 4.0 * mwidth
+                    msmin = mmes0 - 4.0 * mwidth
+                    
+                    almin = np.arctan(-(mmes0**2 - msmin**2) / mwidth / mmes0)
+                    almax = np.arctan(-(mmes0**2 - msmax**2) / mwidth / mmes0)
+                    
+                    al1 = almin + (almax - almin) * rm1
+                    al2 = almin + (almax - almin) * rm2
+                    
+                    mmes1 = np.sqrt(np.tan(al1) * mmes0 * mwidth + mmes0**2)
+                    mmes2 = np.sqrt(np.tan(al2) * mmes0 * mwidth + mmes0**2)
+                    
+                    weight = weight * (almax - almin)**2
+                    weight = weight * mwidth**2 * mmes0**2
+                    weight = weight * (1.0 + np.tan(al1)**2) * (1.0 + np.tan(al2)**2)
+                    weight = weight / 4.0 / mmes1 / mmes2
+                    
+                    mwidth1 = mwidth * ((1.0 - 4.0 * 0.493677**2 / mmes1**2) /
+                                       (1.0 - 4.0 * 0.493677**2 / mmes0**2))**1.5
+                    mwidth2 = mwidth * ((1.0 - 4.0 * 0.493677**2 / mmes2**2) /
+                                       (1.0 - 4.0 * 0.493677**2 / mmes0**2))**1.5
+                    
+                    if mmes1 >= 2.0 * 0.493677 and mmes2 >= 2.0 * 0.493677:
+                        break
+                
+                weight = weight * 2.0 * mmes0 * mmes1 * mwidth1 / pi
+                weight = weight * 2.0 * mmes0 * mmes2 * mwidth2 / pi
+                weight = weight * mmes1**2 * mmes2**2 / mmes0**4
+                weight = weight / ((mmes0**2 - mmes1**2)**2 + mmes1**4 * mwidth1**2 / mmes0**2)
+                weight = weight / ((mmes0**2 - mmes2**2)**2 + mmes2**4 * mwidth2**2 / mmes0**2)
+            
+            # pi rapidities
+            ranx1 = np.random.random()
+            ranx2 = np.random.random()
+            ranx3 = np.random.random()
+            ranx4 = np.random.random()
+            
+            phix1 = 2.0 * pi * ranx1
+            
+            ptxsqmin = 0.0
+            ptxsqmax = 10.0  # generate linear p_t^2
+            
+            ptxsq1 = ptxsqmin + (ptxsqmax - ptxsqmin) * ranx2
+            
+            q[0, 4] = np.sqrt(ptxsq1) * np.cos(phix1)
+            q[1, 4] = np.sqrt(ptxsq1) * np.sin(phix1)
+            q[0, 5] = -pt1[0] - pt2[0] - q[0, 4]
+            q[1, 5] = -pt1[1] - pt2[1] - q[1, 4]
+            ptxsq2 = q[0, 5]**2 + q[1, 5]**2
+            
+            rmx1 = np.sqrt(mmes1**2 + ptxsq1)
+            rmx2 = np.sqrt(mmes2**2 + ptxsq2)
+            
+            ymax1 = np.log(rts / rmx1)
+            
+            if args.cuts == 'true':
+                if rmax < ymax1:
+                    ymax1 = rmax
+            
+            ymin1 = -ymax1
+            
+            yx1 = ymin1 + (ymax1 - ymin1) * ranx3
+            
+            ymax2 = np.log((rts - rmx1 * np.exp(yx1)) / rmx2)
+            ymin2 = -np.log((rts - rmx1 * np.exp(-yx1)) / rmx2)
+            
+            if args.cuts == 'true':
+                if ymax2 > rmax:
+                    ymax2 = rmax
+                if ymin2 < -rmax:
+                    ymin2 = -rmax
+            
+            if ymax2 < ymin2:
+                weight = 0.0
+                sum += weight
+                sum1 += weight**2
+                continue
+            
+            yx2 = ymin2 + (ymax2 - ymin2) * ranx4
+            x1 = (rmx2 * np.exp(yx2) + rmx1 * np.exp(yx1)) / rts
+            x2 = (rmx2 * np.exp(-yx2) + rmx1 * np.exp(-yx1)) / rts
+            
+            weight = weight * (ptxsqmax - ptxsqmin)
+            weight = weight * (ymax1 - ymin1) * (ymax2 - ymin2)
+            
+            q[2, 4] = rmx1 * (np.exp(yx1) - np.exp(-yx1)) / 2.0
+            q[3, 4] = rmx1 * (np.exp(yx1) + np.exp(-yx1)) / 2.0
+            
+            q[2, 5] = rmx2 * (np.exp(yx2) - np.exp(-yx2)) / 2.0
+            q[3, 5] = rmx2 * (np.exp(yx2) + np.exp(-yx2)) / 2.0
+            
+            # Impose massive on-shell condition
+            # p1+ + cc1/p2- = aa1
+            # p2- + cc2/p1+ = aa2 
+            aa1 = bp * (1.0 - x1)
+            aa2 = bp * (1.0 - x2)
+            cc1 = 0.5 * (pt2sq + mp**2)
+            cc2 = 0.5 * (pt1sq + mp**2)
+            
+            root1sq = (cc1 - cc2 - aa1 * aa2)**2 - 4.0 * cc2 * aa1 * aa2
+            root2sq = (cc2 - cc1 - aa1 * aa2)**2 - 4.0 * cc1 * aa1 * aa2
+            
+            if root1sq <= 0.0 or root2sq <= 0.0:
+                weight = 0.0
+                # line 1123 for the GOTO 700 then begin loop again
+                sum += weight
+                sum1 += weight**2
+                continue
+            
+            p1p = (cc2 - cc1 + aa1 * aa2 + np.sqrt(root1sq)) / (2.0 * aa2)
+            p2m = (cc1 - cc2 + aa1 * aa2 + np.sqrt(root2sq)) / (2.0 * aa1)
+            p1m = (pt1sq + mp**2) / (2.0 * p1p)
+            p2p = (pt2sq + mp**2) / (2.0 * p2m)
+            
+            if p1p < 0.0 or p1m < 0.0 or p2p < 0.0 or p2m < 0.0:
+                weight = 0.0
+                sum += weight
+                sum1 += weight**2
+                continue
+            
+            t1 = -rts * p1m * rt2
+            t2 = -rts * p2p * rt2
+            
+            q[:, 2] = [pt1[0], pt1[1], (p1p - p1m) / rt2, (p1p + p1m) / rt2]
+            q[:, 3] = [pt2[0], pt2[1], (p2p - p2m) / rt2, (p2p + p2m) / rt2]
+            
+            svec = q[:, 4] + q[:, 5]
+            sh = np.dot(svec, svec)
+            
+            # TODO: Implement from line 854 onwards
+            
+    
 
 main()
